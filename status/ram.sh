70d7f8173e50434a066d820a3ca4e2fa1458af01
Tai Tran <transytai12a1@gmail.com>
2024-11-14 21:30:59 +0700

    feat: add new status for git branch

--- status/ram.sh
+++ status/ram.sh
@@ -4,7 +4,6 @@ show_ram() {
   local index=$1
   local icon=$(get_tmux_option "@catppuccin_ram_icon" " ")
   local color=$(get_tmux_option "@catppuccin_ram_color" "$thm_green")
-
   local text=$(get_tmux_option "@catppuccin_ram_text" "#{ram_percentage} ")
 
   local module=$(build_status_module "$index" "$icon" "$color" "$text")
